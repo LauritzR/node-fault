ca3637dddf3e9289c94f4a706d99536f64233345
Lauritz Rasbach <rasbachlauritz@googlemail.com>
2019-08-14 09:44:15 +0200

    feature(oom): added out of memory fault

--- nodefault.py
+++ nodefault.py
@@ -1,5 +1,6 @@
 import os
 import json
+import psutil
 
 def setup(config):
    for e in config['node_discover']['args']:
@@ -7,6 +8,7 @@ def setup(config):
       username = e['auth']['username']
       key = e['auth']['private_key_file']
       os.system("scp -r -i %s ops %s@%s:." % (key, username, ip)) 
+      os.system("ssh -i %s %s@%s sudo apt-get install stress" % (key,username,ip)) 
 
 
 def nodeop(config, command):
@@ -18,6 +20,8 @@ def nodeop(config, command):
       op = 'freeze'
    elif 'reset' in command:
       op = 'reset'  
+   elif 'oom' in command:
+      op = 'oom'
    else:
       print('Please specify an operation.')
 
@@ -28,18 +32,17 @@ def nodeop(config, command):
 
       
       if 'all' in command:
-         print("%s %s" % (op, ip))
          try:
             os.system("ssh -i %s %s@%s ./ops/%s.sh" % (key, username, ip, op)) 
          except:
-            print()
+            print("%s %s" % (op, ip))
 
       elif e['fqdn'] in command:
          print("%s %s" % (op, ip))
          try:
-            os.system("ssh -i %s %s@%s ./ops/%s.sh" % (key, username, ip, op)) 
+            os.system("ssh -t -i %s %s@%s screen -m -d ./ops/%s.sh" % (key, username, ip, op)) 
          except:
-            print()
+            print("%s %s" % (op, ip))
    
 
 
