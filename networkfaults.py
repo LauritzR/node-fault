0f2cdb8c328ad6d4cc994a1bc3fcfaf45fee34d3
Lauritz Rasbach <rasbachlauritz@googlemail.com>
2019-08-20 15:58:20 +0200

    feat(nw): final slow implementation

--- networkfaults.py
+++ networkfaults.py
@@ -4,34 +4,33 @@ import psutil
 
 
 def drop(command, ip, username, key):
-    on = command.partition(' ')[1]
-    for e in command.partition(' '):
+    for e in command.split():
         perc = ' '
         if '%' in e:
-            perc = e
+            perc = e.strip()
+        
     os.system("ssh -i %s %s@%s sudo tc qdisc change dev eno1 root netem loss %s>&-" % (key, username, ip, perc))
             
 
 def slow(command, ip, fqdn, username, key):
     if ip in command or fqdn in command:
         print(ip)
-        on = command.partition(' ')[1]
-        for e in command.partition(' '):
+        for e in command.split():
             perc = ' '
             if 'ms' in e:
                 delay = e
         try:
             #print('slow with %s delay' % (delay))
-            os.system("ssh -i %s %s@%s sudo tc qdisc change dev eth0 root netem delay %s>&-" % (key, username, ip, delay))
+            os.system("ssh -i %s %s@%s sudo tc qdisc add dev eno1 root netem delay %s>&-" % (key, username, ip, delay))
         except:
             print() 
     elif 'all' in command:
-        for e in command.partition(' '):
+        for e in command.split():
             perc = ' '
             if 'ms' in e:
                 delay = e
         try:
-            #print('slow with %s delay' % (delay))
-            os.system("ssh -i %s %s@%s sudo tc qdisc change dev eth0 root netem delay %s>&-" % (key, username, ip, delay))
+            print('slow with %s delay' % (delay))
+            os.system("ssh -i %s %s@%s sudo tc qdisc add dev eno1 root netem delay %s>&-" % (key, username, ip, delay))
         except:
             print() 
