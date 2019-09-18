3f1b9d4dc5cc8db89b26bf092d010de424361046
Lauritz Rasbach <rasbachlauritz@googlemail.com>
2019-09-18 08:56:13 +0200

    fix(network): fixed delay for all in slow

--- networkfaults.py
+++ networkfaults.py
@@ -14,18 +14,14 @@ def drop(command, ip, fqdn, username, key):
             
 
 def slow(command, ip, fqdn, username, key):
+    delay = command.split()[2]
     if ip in command or fqdn in command:
-      
-        delay = command.split()[2]
         try:
             print('slow %s with %s delay' % (fqdn, delay))
             os.system("ssh -i %s %s@%s sudo tc qdisc add dev eno1 root netem delay %s" % (key, username, ip, delay))
         except:
             print
     elif 'all' in command:
-        for e in command.split():
-            if 'ms' in e:
-                delay = command.partition(' ')[2]
         try:
             print('slow with %s delay' % (delay))
             os.system("ssh -i %s %s@%s sudo tc qdisc add dev eno1 root netem delay 100ms" % (key, username, ip, delay))
